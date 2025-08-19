f398ffda884acf2af0f5f76fc6406e316f01d908
Michaeldnazario <michaeldnazario@gmail.com>
2025-08-19 19:25:36 -0300

    atividade 2 corrigida

---
+++ atividade2_com_correcao.js
@@ -0,0 +1,35 @@
+function soma(a, b) {
+
+    return a + b;
+  }
+  
+  function subtrai(a, b) {
+
+    return (a - b);
+  }
+  
+  function multiplica(a, b) {
+    return a * b;
+  }
+  
+  function divide(a, b) {
+    if (b === 0) throw new Error("Divisão por zero");
+    return a / b;
+  }
+  
+  // Exporte para Node/CommonJS
+  module.exports = { soma, subtrai, multiplica, divide };
+  
+  // Execução rápida (node calculadora2.js 7 3)
+  if (require.main === module) {
+    const a = Number(process.argv[2] || 0);
+    const b = Number(process.argv[3] || 0);
+    console.log(JSON.stringify({
+      a, b,
+      soma: soma(a,b),
+      subtrai: subtrai(a,b),
+      multiplica: multiplica(a,b),
+      divide: b === 0 ? "erro: divisão por zero" : divide(a,b)
+    }, null, 2));
+  }
+  
